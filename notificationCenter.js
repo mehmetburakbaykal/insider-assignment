1a2895e4a0097432b7b76ba79e0203eccca394e3
Burak Baykal <mehmetburakbaykal1@gmail.com>
2022-11-19 00:47:46 +0300

    needed design

--- notificationCenter.js
+++ notificationCenter.js
@@ -16,114 +16,125 @@ function init() {
   if (!isProductPage && products.length >= 3) {
     const notificationTemplate = ` 
     <div class="notification-center">
-    <div class="notification-container">
-      <h2 class="title">Size Özel İndirimleri Keşfedin</h2>
-      <ul class="notifications">
-      ${lastThreeProducts.map((product) => {
-        return `<li class="notification" onclick="window.location.href='${
-          product.url
-        }'">
-            <img
-              src="${product.product_image_url}"
-              alt="${product.name}"
-            />
-            <div class="desc-container">
-              <h3 class="notification-title">${product.name}</h3>
-              <p class="description">
-                Yalnızca size özel ${product.name} %${
-          Math.floor(Math.random() * 40) + 1
-        } İNDİRİMLİ!
-              </p>
-            </div>
-          </li>`;
-      })}
-      </ul>
-    </div>
-    <div class="button-container">
-      <button class="hide-btn btn">
-        <i class="fa-solid fa-x fa-xl"></i>
-      </button>
-      <button class="ringbell-btn btn hidden">
-        <i class="fa-solid fa-bell fa-xl"></i>
-      </button>
-    </div>
+      <div class="notification-container">
+        <h2 class="title">Size Özel İndirimleri Keşfedin</h2>
+        <ul class="notification-list">
+           ${lastThreeProducts.map((product) => {
+              return `<li class="notification" onclick="window.location.href='${product.url
+                }'">
+                      <img
+                        src="${product.product_image_url}"
+                        alt="${product.name}"
+                      />
+                      <div class="desc-container">
+                        <h3 class="notification-title">${product.name}</h3>
+                        <p class="description">
+                          Yalnızca size özel ${product.name} %${Math.floor(Math.random() * 40) + 10
+                } İNDİRİMLİ!
+                        </p>
+                      </div>
+                    </li>`
+            }).join("")}
+        </ul>
+      </div>
+      <div class="button-container">
+        <button class="hide-btn btn">
+          <i class="fa-solid fa-x fa-xl"></i>
+        </button>
+        <button class="ringbell-btn btn hidden">
+          <i class="fa-solid fa-bell fa-xl"></i>
+        </button>
+      </div>
     </div>
     <style>
-    @import url("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css");
+      @import url("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css");
 
-    * {
-      margin: 0;
-      padding: 0;
-      box-sizing: border-box;
-    }
-    
-    body {
-      background-color: lightgray;
-    }
-    
-    li {
-      list-style: none;
-      cursor: pointer;
-    }
-    
-    img {
-      max-width: 24%;
-      max-height: 24%;
-    }
-    
-    .notification-center {
-      width: 400px;
-      height: 470px;
-      position: fixed;
-      top: 100px;
-      right: 40px;
-      z-index: 9999;
-    }
-    
-    .notification-container {
-      background-color: #ffffff;
-    }
-    
-    .title {
-      width: auto;
-      height: 50px;
-      background-color: rgb(255, 0, 85);
-      padding: 5px;
-      border-radius: 5px 5px 0 0;
-    }
-    
-    .notification {
-      display: flex;
-      padding: 10px;
-    }
-    
-    .desc-container {
-      padding: 10px;
-    }
-    
-    .notification-title {
-      padding-bottom: 10px;
-    }
-    
-    .button-container {
-      position: fixed;
-      top: 550px;
-      right: 10px;
-    }
-    
-    .btn {
-      color: #ffffff;
-      background-color: rgb(255, 0, 85);
-      width: 50px;
-      height: 50px;
-      border: none;
-      border-radius: 50%;
-      cursor: pointer;
-    }
-    
-    .hidden {
-      display: none;
-    }
+      * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+      }
+      
+      body {
+        background-color: lightgray;
+      }
+
+      .notification-center {
+        width: 400px;
+        height: 470px;
+        position: fixed;
+        top: 100px;
+        right: 40px;
+        z-index: 9999;
+      }
+      
+      .notification-container {
+        background-color: #ffffff;
+        border-radius: 10px;
+      }
+
+      .notification-list {
+        padding: 10px;
+      }
+
+      .notification-title {
+        padding-bottom: 10px;
+      }
+
+      .notification {
+        display: flex;
+        list-style: none;
+        cursor: pointer;
+      }
+
+      .notification:not(:last-child) {
+        margin-bottom: 15px;
+      }
+
+      img {
+        max-width: 24%;
+        max-height: 24%;
+        border-radius: 5px;
+      }
+      
+      .title {
+        width: auto;
+        height: 50px;
+        color: #FFFFFF;
+        background-color: #193DB0;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 5px;
+        border-radius: 5px 5px 0 0;
+      }
+
+      .desc-container {
+        display: flex;
+        flex-direction: column;
+        height: 140px;
+        padding: 10px;
+      }
+
+      .button-container {
+        position: fixed;
+        top: 630px;
+        right: 10px;
+      }
+      
+      .btn {
+        color: #FFFFFF;
+        background-color: #193DB0;
+        width: 50px;
+        height: 50px;
+        border: none;
+        border-radius: 50%;
+        cursor: pointer;
+      }
+      
+      .hidden {
+        display: none;
+      }
     </style>
     `;
 
